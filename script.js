b4d34cbe1a973c718ea9c3eec4992b9ca517ce71
Nico Ventura <nicolas.ventura@thebridgeschool.es>
2025-02-24 10:28:47 +0100

    Funcionalidad de busqueda

--- script.js
+++ script.js
@@ -29,14 +29,30 @@ const pokemons = [
   },
 ];
 
+const inputBusqueda = document.getElementById("busquedaPokemon");
 const listaPokemon = document.getElementById("listaPokemon");
 
-pokemons.forEach((pokemon) => {
-  const itemPokemon = document.createElement("li");
-  itemPokemon.innerHTML = `
-  <hr/>
-  <p><b>Pokemon:</b> ${pokemon.name}</p>
-  <p><b>Tipo:</b> ${pokemon.type}</p>
-  `;
-  listaPokemon.appendChild(itemPokemon);
-});
+const mostrarPokemons = (listaObjetosPokemons) => {
+  listaPokemon.innerHTML = "";
+  listaObjetosPokemons.forEach((pokemon) => {
+    const itemPokemon = document.createElement("li");
+    itemPokemon.innerHTML = `
+    <hr/>
+    <p><b>Pokemon:</b> ${pokemon.name}</p>
+    <p><b>Tipo:</b> ${pokemon.type}</p>
+    `;
+    listaPokemon.appendChild(itemPokemon);
+  });
+};
+
+const buscarPokemon = (event) => {
+  const valorBusqueda = event.target.value.toLowerCase();
+  const pokemonsFiltrados = pokemons.filter((pokemon) => {
+    return pokemon.name.toLowerCase().includes(valorBusqueda);
+  });
+  mostrarPokemons(pokemonsFiltrados);
+};
+
+inputBusqueda.addEventListener("keyup", buscarPokemon);
+
+mostrarPokemons(pokemons);
